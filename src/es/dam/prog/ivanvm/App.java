df2e1365c598eb8bbc0c00d532e81958a29d16eb
toxycgo <vicentemorales.ivan@tiernoparla.es>
2023-03-24 09:14:08 +0100

    Calculadora funcionando - IvanVM

---
+++ src/es/dam/prog/ivanvm/App.java
@@ -0,0 +1,58 @@
+package es.dam.prog.ivanvm;
+
+import java.util.Scanner;
+
+import es.dam.prog.ivanvm.calculadora.Calculable;
+import es.dam.prog.ivanvm.calculadora.Calculadora;
+
+public class App {
+
+    private static int inputInt(String msg) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(msg);
+        return input.nextInt();
+    }
+
+    private static double inputDouble(String msg) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(msg);
+        return input.nextDouble();
+    }
+
+    private static int imprimirMenu() {
+        final String MENU_FORMAT = "[1]. Sumar naturales.\n[2]. Sumar reales.\n[3]. Restar reales.\n[4]. Multiplicar reales.\n[5]. Dividir reales.\n\nElige una opción: ";
+    
+        return inputInt(MENU_FORMAT);
+    }
+
+    
+    public static void main(String[] args) {
+        final String INPUT_INT      = "Introduce un número entero: ";
+        final String INPUT_DOUBLE   = "Introduce un número double: ";
+        final String DESPEDIDA      = "¡Adiós!";
+        Calculable calc = new Calculadora();
+
+        int opcion = imprimirMenu();
+
+        switch(opcion) {
+            case 1:
+                calc.sumar(inputInt(INPUT_INT), inputInt(INPUT_INT));
+                break;
+            case 2:
+                calc.sumar(inputDouble(INPUT_DOUBLE), inputDouble(INPUT_DOUBLE));
+                break;
+            case 3:
+                calc.restar(inputDouble(INPUT_DOUBLE), inputDouble(INPUT_DOUBLE));
+                break;
+            case 4:
+                calc.multiplicar(inputDouble(INPUT_DOUBLE), inputDouble(INPUT_DOUBLE));
+                break;
+            case 5:
+                calc.dividir(inputDouble(INPUT_DOUBLE), inputDouble(INPUT_DOUBLE));
+                break;
+            default:
+                System.out.println(DESPEDIDA);
+                break;
+        }
+    }
+}
---
+++ src/es/dam/prog/ivanvm/calculadora/Calculable.java
@@ -0,0 +1,9 @@
+package es.dam.prog.ivanvm.calculadora;
+
+public interface Calculable {
+    public int sumar(int x, int y);
+    public double sumar(double x, double y);
+    public double restar(double x, double y);
+    public double multiplicar(double x, double y);
+    public double dividir(double x, double y);
+}
